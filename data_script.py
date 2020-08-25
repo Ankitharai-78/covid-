fd99c8a96b4b83b756b4905ab520ac2394d065df
Ankitharai-78 <ankitharaik78@gmail.com>
2020-08-25 02:24:55 -0400

    Add changes for base functionality

---
+++ data_script.py
@@ -0,0 +1,22 @@
+import os
+import json
+import requests
+
+if not os.path.exists('data'):
+    os.mkdir('data')
+base_url="https://covid19api.com/"
+def base():
+    """
+    Gets the data from the base url
+    """
+    data={}
+    req=requests.get(base_url)
+    if req.status_code==200:
+        data=(json.loads(req.text))
+        # print(data.keys())
+    return data
+
+if __name__== "__main__":
+  print(base())
+
+
